6177fc0b1dd0145ea1467d943aa62627e1b7723c
YanderuxTea <TeaWithSuqar@yandex.ru>
2025-02-21 18:31:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/; HttpOnly; Secure; SameSite=Strict";
 }
 
 function getCookie(name) {
@@ -55,7 +55,7 @@ function getCookie(name) {
 }
 
 function eraseCookie(name) {
-    document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT; HttpOnly; Secure; SameSite=Strict';
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
